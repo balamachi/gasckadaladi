3aaaab43b3a1a41562b630d3c49be84f393651c8
balamachi <141099816+balamachi@users.noreply.github.com>
2023-08-01 08:21:07 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scal=1.0">
+	
+	<center>
+	
+	<title>Sample Form</title>
+    <h1>Sample form</h1></head>
+	
+    <body bgcolor="FBB917">
+        <div><h2>Sample Entry</h2></div>
+        <form action="Connect.php", method="POST">
+            <label for="user">SNo:</label>
+            <input type="Text" name="SNo" id="SNo" required /><br><br>
+            <label for="user">SName:</label>
+            <input type="Text" name="SName" id="SName" required /><br><br>
+            <input type="Submit" name="Submit" id="Submit"/>
+        </form>
+    </body>
+	
+	</center>
+</html>
+
+
+
